e51816164e0bec8c2c7facd6101fe79415e1e514
Andrew <acolabella11@gmail.com>
2020-04-04 17:03:04 -0400

    quantity modifiers, punctuation, command line args

--- madlib/madlib.py
+++ madlib/madlib.py
@@ -2,15 +2,18 @@
 # coding: utf8
 # CS4120 NLP, Northeastern University 2020
 
+import sys
 import json
 import random
 
 menus = "../webscraping/menus.json"
 ten_percent = range(0,10)
 twenty_five_percent = range(0,4)
-filler = ["Uh", "Um", "Uhhhh", "Yeah", "Hmm"] # informal filler words
+filler = ["Uh", "Um", "Uhhhh", "Yeah", "Hmm"]
+quantities = ["one", "two", "three", "four"]
 like = "like"
-templates = ["Can I get", "Could I get", "Could I just get", "I'll have", "I'd like", "I'll get", "Can I have", "I'll just have", "I'll just get", "Could I please have", "Can I please get", "Can I please have", "Could I please just get", "Could I please just have"]
+templates = ["Can I get", "Could I get", "Could I just get", "I'll have", "I'd like", "I'll get", "Can I have", "I'll just have", "I'll just get", "Could I please have", "Can I please get", "Can I please have", "Could I please just get", "Could I please just have", "I would like", "I will have", "I will get", "Could I please have", "Can I please get", "I would like to have", "I would like to get"]
+modify_templates = ["Actually wait, could you make it", "Could you actually make it", "Could you change it to", "Can you actually change it to", "Wait, can it be", "Wait, can you make it", "Wait, could you make it", "Wait, could you change it to", "And can it be", "Sorry, could you change it to", "And can you change it to", "Sorry, could it be", "Sorry can it be"]
 also = "also"
 conjunctions = ["and", "with"]
 thanks = ["please", "thanks", "thank you", "that's it", "that'll be it", "that's all", "I think that's it", "that should be it", "that should be all", "that's it, I think", "that should be all, I think", "alright, that's it", "alright, that'll be it", "okay, that should be it"]
@@ -32,17 +35,20 @@ def quantities_weighted():
     x = random.choice(range(0,1000))
 
     if x <= 5:
-        return "five"
+        return "Five"
     if x <= 25:
-        return "four"
+        return "Four"
     if x <= 50:
-        return "three"
+        return "Three"
     if x <= 100:
-        return "two"
+        return "Two"
     if x <= 500:
-        return "a"
+        return "A"
     else:
-        return "one"
+        return "One"
+
+def quantities_unweighted():
+    return random.choice(quantities)
 
 # randint between 1 and 2
 def flip_coin():
@@ -83,12 +89,16 @@ def get_item(j, ks):
         
     item = random.choice(range(0, len(m)))
     item_name = list(m[item].keys())[0]
+    item_size = m[item][item_name]["size"]
     
-    return item_name
+    return [item_name, item_size]
 
 def sentence(j, ks):
     sentence = ""
     orders = sentence_order_weighted()
+    modify_item = False
+    modified_item = ""
+    original_q = ""
 
     for i in range(0, orders):
         if twentyfiveP():
@@ -103,15 +113,41 @@ def sentence(j, ks):
         if twentyfiveP():
             sentence += random.choice(filler) + " "
 
-        sentence += quantities_weighted() + " "
+        if tenP():
+            sentence += like + " "
 
-        sentence += get_item(j, ks) + " "
+        q = quantities_weighted()
+        sentence += q + " "
+
+        item = get_item(j, ks)
+        if item[1] != "" and flip_coin():
+            sentence += item[1] + " "
+
+        if tenP() and not modify_item:
+            modify_item = True
+            modified_item = item[0]
+            original_q = q.lower()
+
+        sentence += item[0] + " "
 
         if i < (orders - 1):
             sentence += random.choice(conjunctions) + " "
 
-    if random.choice(twenty_five_percent) == 1:
+    if modify_item:
+        sentence = sentence[:-1] + ". "
+        sentence += random.choice(modify_templates) + " "
+
+        new_q = quantities_unweighted()
+        while new_q == original_q:
+            new_q = quantities_unweighted()
+            
+        sentence += new_q + " "
+        sentence += modified_item + " "
+
+    if twentyfiveP():
         sentence += random.choice(thanks) + " "
+
+    sentence = sentence[:-1] + "."
 
     return sentence
 
@@ -121,10 +157,10 @@ def full_sample(j, ks):
     for i in range(0, sentences):
         print(sentence(j, ks))
 
-def main():
+def main(x):
     j = open_json(menus)
-    for i in range(0, 100):
+    for i in range(0, x):
         full_sample(j, j.keys())
 
 if __name__ == "__main__":
-    main()
+    main(int(sys.argv[1]))
